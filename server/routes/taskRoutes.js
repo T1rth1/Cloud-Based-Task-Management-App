5e0499b2f73795caeb20e4503bb53f95f2ef62dc
Tirth Patel <123004308+T1rth1@users.noreply.github.com>
2024-07-15 23:15:00 +0530

    Update taskRoutes.js

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -16,21 +16,21 @@ import {
 
 const router = express.Router();
 
-router.post("/create", protectRoute, isAdminRoute, createTask); // in frontend side when we make the api request on this api/task/create route then it is render this createTask function or middleware which is created for doing the specific task...
+router.post("/create", isAdminRoute, createTask); // in frontend side when we make the api request on this api/task/create route then it is render this createTask function or middleware which is created for doing the specific task...
 router.post("/duplicate/:id", protectRoute, isAdminRoute, duplicateTask);
 // this is for the /duplicate/:id..means dynamic id..so when we make the api request from the client side then we should pass this id as a dynamic...
 // and we use this id here to do the some operation in the duplicateTask function
-router.post("/activity/:id", protectRoute, postTaskActivity);
+router.post("/activity/:id", postTaskActivity);
 
-router.get("/dashboard", protectRoute, dashboardStatistics);
+router.get("/dashboard", dashboardStatistics);
 router.get("/", protectRoute, getTasks);
 router.get("/:id", protectRoute, getTask);
 
-router.put("/create-subtask/:id", protectRoute, isAdminRoute, createSubTask);
+router.put("/create-subtask/:id", isAdminRoute, createSubTask);
 router.put("/update/:id", protectRoute, isAdminRoute, updateTask);
 router.put("/:id", protectRoute, isAdminRoute, trashTask);
 
-router.delete("/delete-restore/:id?",protectRoute,isAdminRoute,deleteRestoreTask);
+router.delete("/delete-restore/:id?",isAdminRoute,deleteRestoreTask);
 
 // here we make the every route as a protect route means user must need to be authenticated or verified...if he/she real or not
 // and we pass the isAdminRoute middlware at some routes on which some admin activity would be performed...
