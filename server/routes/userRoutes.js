73e4b5bf896f4adff10638e0804a5b3c1e9c5f7d
Tirth Patel <123004308+T1rth1@users.noreply.github.com>
2024-07-15 23:23:09 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -19,15 +19,15 @@ router.post("/register",registerUser);
 router.post("/login",loginUser);
 router.post("/logout",logoutUser);
 
-router.get("/get-team",isAdminRoute,getTeamList);
-router.get("/notifications",getNotificationsList);
+router.get("/get-team",protectRoute,isAdminRoute,getTeamList);
+router.get("/notifications",protectRoute,getNotificationsList);
 
-router.put("/profile",updateUserProfile);
-router.put("/read-noti",markNotificationRead);
-router.put("/change-password",changeUserPassword);
+router.put("/profile",protectRoute,updateUserProfile);
+router.put("/read-noti",protectRoute,markNotificationRead);
+router.put("/change-password",protectRoute,changeUserPassword);
 
 //   FOR ADMIN ONLY - ADMIN ROUTES
-router.route("/:id").put(isAdminRoute, activateUserProfile).delete(protectRoute, isAdminRoute, deleteUserProfile);
+router.route("/:id").put(protectRoute, isAdminRoute, activateUserProfile).delete(protectRoute, isAdminRoute, deleteUserProfile);
 // this is admin route because we activate and delete user profile if we are the admin for this only we needed the id from the frontend
 // of the user which admin want to delete and activeate and deactivate the profile
 
